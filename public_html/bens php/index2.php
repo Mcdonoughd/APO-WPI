711f3c92b4d17bb4c5d033e7ca1e3e65600210f5
Mcdonoughd <dmcdonough@wpi.edu>
2018-01-19 15:01:36 -0500

    updated archive

---
+++ public_html/bens php/index2.php
@@ -0,0 +1,133 @@
+<!--<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">-
+<html xmlns="http://www.w3.org/1999/xhtml">-->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Alpha Phi Omega</title>
+<link href="styles.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+<!--
+.style1 {
+	font-size: 9px;
+	font-style: italic;
+}
+.style2 {
+	font-size: 16px;
+	font-weight: bold;
+}
+.style3 {font-size: 18px}
+.style4 {font-size: 10px}
+.style5 {
+	font-size: 14px
+}
+-->
+</style>
+</head>
+
+<body>
+<!-- Save for Web Slices (template.psd) -->
+<table width="850" height="900" border="0" align="center" cellpadding="0" cellspacing="0" id="Table_01">
+<tr>
+		<td height="108" colspan="2" align="center">
+			<img src="img/template_02.jpg" alt="" width="815" height="108"></td>
+  </tr>
+	<tr>
+		<td height="57" align="center">
+			<img src="img/template_05.jpg" width="814" height="57" alt=""></td>
+  </tr>
+	<tr>
+		<td height="5">&nbsp;</td>
+  </tr>
+	<tr>
+	  <td align="center" valign="top" id="mainCell2">
+<table width="95%" border="0" cellpadding="0" cellspacing="0" id="mainTable">
+        <tr>
+          <td width="269" height="200" valign="top"><br>
+            <table width="250" border="0" cellpadding="0" cellspacing="0" class="topNews">
+              <tr>
+                <td class="topnewsBottom style2"><span class="style3">Welcome!</span></td>
+              </tr>
+              
+              <tr>
+                <td><span class="style4"> Welcome to Worcester Polytechnic Institute's Alpha Phi Omega Chapter!</span></td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+              </tr>
+            </table>
+          <!--
+          <div class="left">
+            Welcome to WPI's Alpha Phi Omega Chapter!
+          </div>
+
+          <div class="right">right</div> 
+           <br><br>
+
+          <div class="left">
+          <div align="center">
+            <img src="http://users.wpi.edu/~apo/images/groupphoto.JPG" alt="" width="250" height="200">          
+            </div>
+            </div>    
+            -->          </td>
+          <td width="269" height="200" valign="top">
+          &nbsp;
+          <table width="250" border="0" cellpadding="0" cellspacing="0" class="topNews">
+              <tr>
+                <td class="topnewsBottom style2"><span class="style3">Another news...</span></td>
+              </tr>
+              
+              <tr>
+                <td><span class="style4"> This is also news, but this news is going to be a little longer. I'm going to rabble on and on just for a little demonstration here. I'm now gonna switch to large words like antidisestablishmentarianism and supercalifragilisticexpialidosios which I probably spelled wrong. Anyway, the reason I'm writing this long paragraph is because eventually. right when you think I'm about to reveal the point of the story...</span></td>
+              </tr>
+              <tr>
+                <td><div align="right" class="style1">Click here to read more</div></td>
+              </tr>
+            </table>
+          </td>
+          <td height="200" valign="top">         &nbsp;
+          <table width="250" border="0" cellpadding="0" cellspacing="0" class="topNews">
+              <tr>
+                <td class="topnewsBottom style2"><span class="style3">Welcome!</span></td>
+              </tr>
+              
+              <tr>
+                <td><span class="style4"> Welcome to Worcester Polytechnic Institute's Alpha Phi Omega Chapter!</span></td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+              </tr>
+            </table>
+            </td>
+        </tr>
+        <tr>
+          <td height="250" colspan="2" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <td><h3 class="style5">&nbsp;&nbsp;National Convention '08</h3></td>
+            </tr>
+            
+            <tr>
+              <td><div align="center"><img src="http://users.wpi.edu/~apo/images/groupphoto.JPG" alt="" width="237" height="189" border="1"></div></td>
+            </tr>
+            <tr>
+              <td><div align="center"><span class="style1">APO-OI at the National Convention in Boston, MA, December 27-30, 2008</span></div></td>
+              </tr>
+          </table></td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td height="150">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+      </table>
+      <!--<img src="img/template_07.jpg" width="815" height="689" alt="">--></td>
+  </tr>
+
+</table>
+<!-- End Save for Web Slices -->
+</body>
+</html>
